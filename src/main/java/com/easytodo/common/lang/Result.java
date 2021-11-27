cbc83dad0684f61dac24e512c8f16e2a82f56fd7
Chenshuwen-pro <chenshuwen@bupt.edu.cn>
2021-11-27 21:12:48 +0800

    增加comment功能

--- src/main/java/com/easytodo/common/lang/Result.java
+++ src/main/java/com/easytodo/common/lang/Result.java
@@ -7,12 +7,12 @@ import java.io.Serializable;
 @Data
 public class Result implements Serializable {
 
-    private int code; // 200是正常，非200表示异常
+    private int code; // 0是正常，1表示异常
     private String msg;
     private Object data;
 
     public static Result succ(Object data) {
-        return succ(200, "操作成功", data);
+        return succ(0, "操作成功", data);
     }
 
     public static Result succ(int code, String msg, Object data) {
@@ -24,11 +24,11 @@ public class Result implements Serializable {
     }
 
     public static Result fail(String msg) {
-        return fail(400, msg, null);
+        return fail(1, msg, null);
     }
 
     public static Result fail(String msg, Object data) {
-        return fail(400, msg, data);
+        return fail(1, msg, data);
     }
 
     public static Result fail(int code, String msg, Object data) {
